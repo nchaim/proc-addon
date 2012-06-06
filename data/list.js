e3c3ea2636b761b1a21121dc9b7960cc6897f00d
Nicolas Chaim <nicolas@n1>
2012-06-06 14:01:07 -0400

    Initial commit

---
+++ data/list.js
@@ -0,0 +1,223 @@
+var numProcessors = null;
+var devPrefixes = null;
+var winVer = null;
+
+function enumProcesses(){
+  var res = {};
+  if (typeof enumProcesses.bufferSize == 'undefined' || enumProcesses.bufferSize.value > 0x20000)
+    enumProcesses.bufferSize = new ctypes.unsigned_long(0x4000);
+  var buffer = ctypes.char.array(enumProcesses.bufferSize.value)();
+  while(true){
+    var status = NtQuerySystemInformation(SystemProcessInformation, buffer, 
+      enumProcesses.bufferSize, enumProcesses.bufferSize.address());  
+    if (status == STATUS_BUFFER_TOO_SMALL || status == STATUS_INFO_LENGTH_MISMATCH) {
+      buffer = ctypes.char.array(enumProcesses.bufferSize.value)();   
+    } else break;
+  }
+  if (status < 0) return false;
+  
+  var i = 0;
+  while(true){
+    /*var*/ proc = ctypes.cast(buffer.addressOfElement(i), SYSTEM_PROCESS_INFORMATION.ptr).contents;
+    pid = cvalconv(proc.UniqueProcessId);
+    var pinfo = cstruct2arr(proc);
+    pinfo["IOCount"] = pinfo.ReadOperationCount + pinfo.WriteOperationCount + pinfo.OtherOperationCount;
+    pinfo["IOTransfer"] = pinfo.ReadTransferCount + pinfo.WriteTransferCount + pinfo.OtherTransferCount;
+    if (pid == SYSTEM_IDLE_PROCESS_ID) 
+      pinfo.KernelTime = getProcessorCounters().IdleTime;
+    pinfo["CPUTime"] = pinfo.UserTime + pinfo.KernelTime;
+    res[pid] = pinfo;
+    if (proc.NextEntryOffset == 0) break; 
+    i += parseInt(proc.NextEntryOffset);
+  }
+  return res;
+}
+
+function windowsVersion(){
+  var info = new OSVERSIONINFOW();
+  var status = RtlGetVersion(info.address());   
+  if (status < 0) return false;
+  return parseFloat(info.dwMajorVersion+"."+info.dwMinorVersion);
+}
+
+function hImageFileName60(pid){
+  var strBuf = ctypes.jschar.array(MAX_PATH)();
+  var info = SYSTEM_PROCESS_ID_INFORMATION(pid, UNICODE_STRING(0, MAX_PATH, strBuf));
+  var status = NtQuerySystemInformation(SystemProcessIdInformation, info.address(), 
+    SYSTEM_PROCESS_ID_INFORMATION.size, null);  
+  if (status < 0) return false;
+  return cvalconv(info.ImageName);
+}
+
+function hImageFileName50(pid){
+  var strBuf = ctypes.jschar.array(MAX_PATH)();
+  var handle = OpenProcess(PROCESS_QUERY_INFORMATION, false, pid);
+  var status = GetProcessImageFileNameW(handle, strBuf, MAX_PATH);
+  CloseHandle(handle);
+  if (status == 0) return false;
+  return cvalconv(strBuf); 
+}
+
+function imageFileName(pid){
+  pid = parseInt(pid);
+  if (!winVer) winVer = windowsVersion();
+  var fn = (winVer < 6.0) ? hImageFileName50(pid) : hImageFileName60(pid);
+  if (typeof fn == "string") fn = nt2dosName(fn);
+  return fn;
+}
+
+function nt2dosName(name){
+   if(!devPrefixes) devPrefixes = devicePrefixes();
+   name = name.replace(/^\\\?\\(UNC\\)?/i, "");
+   for(var pfx in devPrefixes){
+     if (name.indexOf(pfx) ==  0)
+      {name = name.replace(pfx, devPrefixes[pfx]); break;}
+   }
+   return name;
+}
+
+function devicePrefixes(){
+  var res = {"\\Device\\Mup": "\\"};
+  var drives = GetLogicalDrives();
+  var buffer = ctypes.jschar.array(0x100)();
+  for(var i = 0, b = 1; i < 26; i++, b <<= 1){
+    if (!(drives & b)) continue;
+    var drv = String.fromCharCode(65+i) + ":";
+    var drvW = ctypes.jschar.array()(drv);
+    var ret = QueryDosDeviceW(drvW, buffer, 0x100);
+    if (ret) res[buffer.readString()] = drv;
+  }
+  return res;
+}
+
+function versionInfo(fname){
+  var verStrs = ["CompanyName", "FileDescription", "FileVersion", "InternalName", 
+    "LegalCopyright", "OriginalFilename", "ProductName", "ProductVersion"];
+  if(!fname || fname.match(/^\\\\/)) return false;
+  var zero =  new ctypes.uint32_t();
+  var fnameW = ctypes.jschar.array()(fname);
+  var len = GetFileVersionInfoSizeW(fnameW, zero.address());
+  if(!len) return false;
+  var buffer = ctypes.char.array(len)();
+  var ret = GetFileVersionInfoW(fnameW,  0, len, buffer);
+  if (!ret) return false;
+  var ptrans = verInfoValue(buffer, "\\VarFileInfo\\Translation");
+  var trans = ctypes.cast(ptrans, LANGANDCODEPAGE.ptr).contents;
+  var fInfoStr = "\\StringFileInfo\\" + dec2hex(trans.wLanguage, 4) + 
+    dec2hex(trans.wCodePage, 4) + "\\";
+  var res = {};
+  for(var i in verStrs) {
+    var rv = verInfoValue(buffer, fInfoStr+verStrs[i]);
+    if (rv) res[verStrs[i]] = rv;
+  }
+  return res;
+}
+
+function verInfoValue(pblock, bstr){
+  var verStr = ctypes.jschar.array()(bstr);
+  var blen = ctypes.unsigned_int();
+  var pbuf = new ctypes.voidptr_t();
+  var res = VerQueryValue(pblock, verStr, pbuf.address(), blen.address());
+  if (!res || blen.value == 0) return false;
+  if (bstr.match(/^\\StringFileInfo/)) 
+    return ctypes.cast(pbuf, ctypes.jschar.ptr).readString();
+  else return pbuf;
+}
+
+function enumProcServices(){
+  var handle = OpenSCManagerW(null, null, SC_MANAGER_CONNECT | SC_MANAGER_ENUMERATE_SERVICE);
+  if (!handle) return false;
+  var bufferSize = new ctypes.uint32_t(0);
+  var numSvc = new ctypes.uint32_t(0);
+
+  var res = EnumServicesStatusExW(handle, SC_ENUM_PROCESS_INFO, SERVICE_WIN32, 
+    SERVICE_ACTIVE, null, 0, bufferSize.address(), numSvc.address(), null, null);
+  var buffer = ctypes.char.array(bufferSize.value)();  
+  res = EnumServicesStatusExW(handle, SC_ENUM_PROCESS_INFO, SERVICE_WIN32, 
+    SERVICE_ACTIVE, buffer, bufferSize, bufferSize.address(), numSvc.address(), null, null);
+  CloseServiceHandle(handle);
+  if (!res) return false; 
+  /*var*/ svcs = ctypes.cast(buffer, ENUM_SERVICE_STATUS_PROCESS.array(numSvc.value));
+  var res = {};
+  for(var i = 0; i < svcs.length; i++){
+    var pid = parseInt(svcs[i].ServiceStatusProcess.dwProcessId.toString());
+    if(pid == 0) continue;
+    var s = cstruct2arr(svcs[i]);
+    if (!(pid in res)) res[pid] = [];
+    res[pid].push(s);  
+  }
+  return res;
+}
+
+function getProcessorCounters(){
+  var res = {};
+  if(!numProcessors) numProcessors = getBasicInfo().NumberOfProcessors;
+  var buffer = SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION.array(numProcessors)();
+  var status = NtQuerySystemInformation(SystemProcessorPerformanceInformation, buffer, 
+    SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION.size*numProcessors, null);  
+  if (status < 0) return false;
+  for(var pn in buffer[0]){
+    res[pn] = 0;
+    for(var i = 0; i < numProcessors; i++)
+      res[pn] += parseInt(buffer[i][pn]);
+  }
+  return res;
+}
+
+function getBasicInfo(){
+  var sysInfo = new SYSTEM_BASIC_INFORMATION();
+  var status = NtQuerySystemInformation(SystemBasicInformation, sysInfo.address(), 
+    SYSTEM_BASIC_INFORMATION.size, null);  
+  if (status < 0) return false;
+  return cstruct2arr(sysInfo);
+}
+
+function cstruct2arr(s){
+  var res = {};
+  for(var prop in s) {
+    var cv = cvalconv(s[prop]);
+    if(typeof cv == "string" && cv.match(/^\w+\(.*,.*\)$/))
+      cv = cstruct2arr(s[prop]);
+    res[prop] = cv;
+  }
+  return res;
+}
+
+function cvalconv(v){
+  var constr = v.constructor.toString();
+  if (typeof v == "number") return v;
+  if (v instanceof ctypes.jschar.ptr || v instanceof ctypes.char.ptr) 
+    return v.isNull() ? "" : v.readString();
+  if (v instanceof UNICODE_STRING) 
+    return v.Buffer.isNull() ? "" : v.Buffer.readString(); 
+  if (v instanceof ctypes.UInt64 || v instanceof ctypes.Int64) 
+    return parseInt(v);
+  if (constr.match(/\*$/)) return v.toString().match(/0x[0-9a-f]+/i)[0];   
+  if (constr.match(/(char|jschar)\[\d+\]$/)) 
+    return v.isNull() ? "" : v.readString();
+  return v.toString();
+}
+
+function dec2hex(num, w){
+  return (Array(w).join("0") + num.toString(16)).substr(-w);
+}
+
+function delta(s1, s2, time){
+  var res = {};
+	var totCPUTime = 0;
+	var deltaList = ["UserTime", "KernelTime", "PageFaultCount", "ReadOperationCount", "WriteOperationCount", 
+    "OtherOperationCount", "ReadTransferCount", "WriteTransferCount", "OtherTransferCount", "IOCount",
+    "IOTransfer", "CPUTime"];
+	for(var pid in s1){
+	  if (!(pid in s2)) continue;
+	  res[pid] = {};
+	  for(var i in deltaList) {
+	    var p = deltaList[i];
+	    res[pid][p+"Delta"] = (s2[pid][p] - s1[pid][p]) / time;
+	  }
+	  totCPUTime += res[pid]["CPUTimeDelta"];
+	} 
+	for(var pid in res)
+    res[pid]["CPU"] = res[pid]["CPUTimeDelta"] / totCPUTime;  
+	return res;
+}
---
+++ data/ntapi.js
@@ -0,0 +1,212 @@
+Components.utils.import("resource://gre/modules/ctypes.jsm"); 
+
+const ntdll = ctypes.open("Ntdll.dll");
+const psapi = ctypes.open("Psapi.dll");
+const kernel32 = ctypes.open("Kernel32.dll");
+const advapi32 = ctypes.open("Advapi32.dll");
+const versiondll = ctypes.open("version.dll");
+
+const UNICODE_STRING = new ctypes.StructType("UNICODE_STRING", [
+  {'Length': ctypes.unsigned_short},
+  {'MaximumLength': ctypes.unsigned_short},
+  {'Buffer': ctypes.jschar.ptr} ]);
+
+const SYSTEM_PROCESS_INFORMATION = new ctypes.StructType("SYSTEM_PROCESS_INFORMATION", [
+  {'NextEntryOffset': ctypes.unsigned_long},
+  {'NumberOfThreads': ctypes.unsigned_long},
+  {'WorkingSetPrivateSize': ctypes.long_long},
+  {'HardFaultCount': ctypes.unsigned_long},
+  {'NumberOfThreadsHighWatermark': ctypes.unsigned_long},
+  {'CycleTime': ctypes.unsigned_long_long},
+  {'CreateTime': ctypes.long_long},
+  {'UserTime': ctypes.long_long},
+  {'KernelTime': ctypes.long_long},
+  {'ImageName': UNICODE_STRING},
+  {'BasePriority': ctypes.long},
+  {'UniqueProcessId': ctypes.unsigned_long},
+  {'InheritedFromUniqueProcessId': ctypes.unsigned_long},
+  {'HandleCount': ctypes.unsigned_long},
+  {'SessionId': ctypes.unsigned_long},
+  {'UniqueProcessKey': ctypes.unsigned_long},
+  {'PeakVirtualSize': ctypes.size_t},
+  {'VirtualSize': ctypes.size_t},
+  {'PageFaultCount': ctypes.unsigned_long},
+  {'PeakWorkingSetSize': ctypes.size_t},
+  {'WorkingSetSize': ctypes.size_t},
+  {'QuotaPeakPagedPoolUsage': ctypes.size_t},
+  {'QuotaPagedPoolUsage': ctypes.size_t},
+  {'QuotaPeakNonPagedPoolUsage': ctypes.size_t},
+  {'QuotaNonPagedPoolUsage': ctypes.size_t},
+  {'PagefileUsage': ctypes.size_t},
+  {'PeakPagefileUsage': ctypes.size_t},
+  {'PrivatePageCount': ctypes.size_t},
+  {'ReadOperationCount': ctypes.long_long},
+  {'WriteOperationCount': ctypes.long_long},
+  {'OtherOperationCount': ctypes.long_long},
+  {'ReadTransferCount': ctypes.long_long},
+  {'WriteTransferCount': ctypes.long_long},
+  {'OtherTransferCount': ctypes.long_long} ]);
+
+const SYSTEM_PROCESS_ID_INFORMATION = new ctypes.StructType("SYSTEM_PROCESS_ID_INFORMATION", [
+  {'ProcessId': ctypes.long},
+  {'ImageName': UNICODE_STRING} ]);
+
+const SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION = new ctypes.StructType("SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION", [
+  {'IdleTime': ctypes.long_long},
+  {'KernelTime': ctypes.long_long},
+  {'UserTime': ctypes.long_long},
+  {'DpcTime': ctypes.long_long},
+  {'InterruptTime': ctypes.long_long},
+  {'InterruptCount': ctypes.unsigned_long} ]);
+  
+const SYSTEM_BASIC_INFORMATION = new ctypes.StructType("SYSTEM_BASIC_INFORMATION", [
+  {'Reserved': ctypes.unsigned_long},
+  {'TimerResolution': ctypes.unsigned_long},
+  {'PageSize': ctypes.unsigned_long},
+  {'NumberOfPhysicalPages': ctypes.unsigned_long},
+  {'LowestPhysicalPageNumber': ctypes.unsigned_long},
+  {'HighestPhysicalPageNumber': ctypes.unsigned_long},
+  {'AllocationGranularity': ctypes.unsigned_long},
+  {'MinimumUserModeAddress': ctypes.unsigned_long.ptr},
+  {'MaximumUserModeAddress': ctypes.unsigned_long.ptr},
+  {'ActiveProcessorsAffinityMask': ctypes.unsigned_long.ptr},
+  {'NumberOfProcessors': ctypes.char} ]);
+
+const OSVERSIONINFOW = new ctypes.StructType("OSVERSIONINFOW", [
+  {'dwOSVersionInfoSize': ctypes.uint32_t},
+  {'dwMajorVersion': ctypes.uint32_t},
+  {'dwMinorVersion': ctypes.uint32_t},
+  {'dwBuildNumber': ctypes.uint32_t},
+  {'dwPlatformId': ctypes.uint32_t},
+  {'usage': ctypes.jschar.array(128)} ]);
+
+const SERVICE_STATUS_PROCESS = new ctypes.StructType("SERVICE_STATUS_PROCESS", [
+  {'dwServiceType': ctypes.uint32_t},
+  {'dwCurrentState': ctypes.uint32_t},
+  {'dwControlsAccepted': ctypes.uint32_t},
+  {'dwWin32ExitCode': ctypes.uint32_t},
+  {'dwServiceSpecificExitCode': ctypes.uint32_t},
+  {'dwCheckPoint': ctypes.uint32_t},
+  {'dwWaitHint': ctypes.uint32_t},
+  {'dwProcessId': ctypes.uint32_t},
+  {'dwServiceFlags': ctypes.uint32_t} ]);
+
+const ENUM_SERVICE_STATUS_PROCESS = new ctypes.StructType("ENUM_SERVICE_STATUS_PROCESS", [
+  {'lpServiceName': ctypes.jschar.ptr},
+  {'lpDisplayName': ctypes.jschar.ptr},
+  {'ServiceStatusProcess': SERVICE_STATUS_PROCESS} ]);
+
+const LANGANDCODEPAGE = new ctypes.StructType("LANGANDCODEPAGE", [
+  {'wLanguage': ctypes.uint16_t},
+  {'wCodePage': ctypes.uint16_t} ]);
+
+const NtQuerySystemInformation = ntdll.declare("NtQuerySystemInformation", 
+  ctypes.winapi_abi,
+  ctypes.long, // return
+  ctypes.int, // SystemInformationClass
+  ctypes.void_t.ptr, // SystemInformation
+  ctypes.unsigned_long, // SystemInformationLength
+  ctypes.unsigned_long.ptr); // ReturnLength
+
+const RtlGetVersion = ntdll.declare("RtlGetVersion", 
+  ctypes.winapi_abi,
+  ctypes.long, // return
+  OSVERSIONINFOW.ptr); // lpVersionInformation
+
+const OpenProcess = kernel32.declare("OpenProcess", 
+  ctypes.winapi_abi,
+  ctypes.voidptr_t, // return
+  ctypes.uint32_t, // dwDesiredAccess
+  ctypes.bool, // bInheritHandle
+  ctypes.uint32_t); // dwProcessId
+
+const GetProcessImageFileNameW = psapi.declare("GetProcessImageFileNameW", 
+  ctypes.winapi_abi,
+  ctypes.uint32_t, // return
+  ctypes.voidptr_t, // hProcess
+  ctypes.jschar.ptr, // lpImageFileName
+  ctypes.uint32_t); // nSize
+
+const CloseHandle = kernel32.declare("CloseHandle", 
+  ctypes.winapi_abi,
+  ctypes.bool, // return
+  ctypes.voidptr_t); // hObject
+
+const EnumServicesStatusExW = advapi32.declare("EnumServicesStatusExW", 
+  ctypes.winapi_abi,
+  ctypes.bool, // return
+  ctypes.void_t.ptr, // hSCManager
+  ctypes.long, // InfoLevel
+  ctypes.uint32_t, // dwServiceType
+  ctypes.uint32_t, // dwServiceState
+  ctypes.char.ptr, // lpServices
+  ctypes.uint32_t, // cbBufSize
+  ctypes.uint32_t.ptr, // pcbBytesNeeded
+  ctypes.uint32_t.ptr, // lpServicesReturned
+  ctypes.uint32_t.ptr, // lpResumeHandle
+  ctypes.jschar.ptr); // pszGroupName
+
+const OpenSCManagerW = advapi32.declare("OpenSCManagerW", 
+  ctypes.winapi_abi,
+  ctypes.void_t.ptr, // return
+  ctypes.jschar.ptr, // lpMachineName
+  ctypes.jschar.ptr, // lpDatabaseName
+  ctypes.uint32_t); // dwDesiredAccess
+
+const CloseServiceHandle = advapi32.declare("CloseServiceHandle", 
+  ctypes.winapi_abi,
+  ctypes.bool, // return
+  ctypes.void_t.ptr); // hSCObject
+
+const QueryDosDeviceW = kernel32.declare("QueryDosDeviceW", 
+  ctypes.winapi_abi,
+  ctypes.uint32_t, // return
+  ctypes.jschar.ptr, // lpDeviceName
+  ctypes.jschar.ptr, // lpTargetPath
+  ctypes.uint32_t); // ucchMax
+
+const GetLogicalDrives = kernel32.declare("GetLogicalDrives", 
+  ctypes.winapi_abi,
+  ctypes.uint32_t); // return
+
+const GetFileVersionInfoSizeW = versiondll.declare("GetFileVersionInfoSizeW", 
+  ctypes.winapi_abi,
+  ctypes.uint32_t, // return
+  ctypes.jschar.ptr, // lptstrFilename
+  ctypes.uint32_t.ptr); // lpdwHandle
+
+const GetFileVersionInfoW = versiondll.declare("GetFileVersionInfoW", 
+  ctypes.winapi_abi,
+  ctypes.bool, // return
+  ctypes.jschar.ptr, // lptstrFilename
+  ctypes.uint32_t, // dwHandle
+  ctypes.uint32_t, // dwLen
+  ctypes.void_t.ptr); // lpData
+
+const VerQueryValue = versiondll.declare("VerQueryValueW", 
+  ctypes.winapi_abi,
+  ctypes.bool, // return
+  ctypes.void_t.ptr, // pBlock
+  ctypes.jschar.ptr, // lpSubBlock
+  ctypes.void_t.ptr.ptr, // lplpBuffer
+  ctypes.unsigned_int.ptr); // puLen
+
+const STATUS_BUFFER_TOO_SMALL = 0xC0000023>>0;
+const STATUS_INFO_LENGTH_MISMATCH = 0xC0000004>>0;
+const SystemProcessInformation = 5;
+const SystemProcessIdInformation = 88;
+const SystemProcessorPerformanceInformation = 8;
+const SystemBasicInformation = 0;
+const SYSTEM_IDLE_PROCESS_ID = 0;
+const SYSTEM_PROCESS_ID = 4;
+const MAX_PATH = 260;
+const PROCESS_QUERY_INFORMATION  = 0x400;
+const PROCESS_VM_READ = 0x10;
+const SC_MANAGER_ENUMERATE_SERVICE = 0x4;
+const SC_MANAGER_CONNECT = 0x1;
+const SC_ENUM_PROCESS_INFO = 0;
+
+const SERVICE_ACTIVE = 0x00000001;
+const SERVICE_WIN32_OWN_PROCESS = 0x00000010;
+const SERVICE_WIN32_SHARE_PROCESS = 0x00000020;
+const SERVICE_WIN32 = SERVICE_WIN32_OWN_PROCESS | SERVICE_WIN32_SHARE_PROCESS;
